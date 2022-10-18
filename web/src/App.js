43ffb983b207472514c32f386f3519020c00acf3
Martin <nitromagix@gmail.com>
2022-10-17 20:43:52 -0700

    refactor

--- web/src/App.js
+++ web/src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CurrentUserProvider from "./contexts/CurrentUser";
 import Layout from "./components/Layout";
-import Home from "./components/Home";
+import Dashboard from "./components/Dashboard";
 import LoginForm from "./components/LoginForm";
 import SignUpForm from "./components/SignUpForm";
 
@@ -13,7 +13,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
+            <Route index element={<Dashboard />} />
             <Route path="login" element={<LoginForm />} />
             <Route path="signup" element={<SignUpForm />} />
             {/* <Route path="*" element={<NoPage />} /> */}
