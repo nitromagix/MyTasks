ec666e20eff875dd030605b742577354dff7caee
Martin <nitromagix@gmail.com>
2022-10-18 21:55:26 -0700

    modified Dashboard page

--- web/src/components/Dashboard.js
+++ web/src/components/Dashboard.js
@@ -1,10 +1,20 @@
 import { useContext, Fragment } from "react";
+import { Link } from "react-router-dom";
+import { CurrentUser } from "../contexts/CurrentUser";
 
 const Dashboard = () => {
+  const userContext = useContext(CurrentUser);
+  const user = userContext.currentUser;
 
-  return (
+  return user && user.role === "reviewer" ? (
     <Fragment>
       <h1>Dashboard</h1>
+      <div style={{ width: 350 }}></div>
+    </Fragment>
+  ) : (
+    <Fragment>
+      <h1>Welcome to MyTasks.</h1>
+      <h2>Please <Link to={"/login"}>log in</Link></h2>
       <div style={{width:350}}></div>
     </Fragment>
   );
